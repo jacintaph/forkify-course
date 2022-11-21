f26135c2d18269865bc13362352260a7ca973de8
JacintaPh <jacintaphillips14@gmail.com>
2022-11-21 11:34:41 +1000

    Cleaned up controller

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -133,9 +133,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
 // Subscriber
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -145,7 +142,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
